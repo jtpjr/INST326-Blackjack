b5bd90e3f03980ba66197f77c2925ac4028cea2d
Jude Poole Jr <judepoolejr@gmail.com>
2020-11-02 20:05:21 -0500

    Added skeleton code

--- blackjack.py
+++ blackjack.py
@@ -1,23 +1,98 @@
+from random import shuffle
+
 class Blackjack:
     """
-    This class creates a command line game of blackjack
+    This class creates a command line game of blackjack against the computer (player vs dealer(CPU))
     """
-    def __init__():
+    def __init__(self, player_purse, player_hand, dealer_hand, pot, master_deck, buy_in=250):
+        """
+        Constructor for the Blackjack game
+
+        Args:
+            player_purse: Current integer value of player's purse optionally set by passed in argument
+            buy_in: Optionally set buy-in value for the player (default 250)
+            pot: Integer holding current bet for current round
+            master_deck: List of 6x Decks comprising tuples representing cards of tuples (suit, value)
+        """
+
+        # Sets player purse to passed buy_in argument
+        self.player_purse = buy_in
+
+        # Sets pot to be an integer to hold current bet for current round
+        self.pot = int
+
+        # Sets the master_deck to a list of 6 individual decks combined
+        self.master_deck = list.append(Deck for i in range(0,6))
+        shuffle(master_deck)
+
+        # Creates an instance_deck, which is a copy of the master deck to be used in play while preserving master_deck
+        self.instance_deck = list(self.master_deck)
+
+        # Sets the player_hand and dealer_hand to lists to hold current cards
+        self.player_hand = list()
+        self.dealer_hand = list()
+
+
+    def draw(self):
+        """
+        Returns: Returns the top card (first card) in the deck
+        """
+        return self.instance_deck.pop(0)
+
+    def round(self):
+
+        self.player_hand.append(draw())
+
+    def bet(self, value):
+
+
+    def fold(self):
+
+
+    def split(self):
+
+
+    def double_down(self):
+
+class Card:
     """
-    constructor for the Blackjack class
+    This class makes a single card, with suit and value
     """
-    
+    def __init__(self, value, suit):
+        """
+        Constructor for card
+
+        Args:
+            value: (int) value of card
+            suit: (str) suit of card
+            name: (str) name of card (2, king, ...)
+        """
+        self.suit = suit
+        self.value = value
+
+        names = {11:"Jack", 12:"Queen", 13:"King", 14:"Ace"}
+
+        if 2 <= value <= 10:
+            self.name = str(value)
+        else:
+            self.name = names[value]
+
 class Deck:
     """
-    This is a deck of cards
+    This is a deck of cards, as a list of tuples (suit, value)
     """
-    def shuffle():
-        """
-        This is a function to randomize the position of cards in the deck
-        """
         
-    def __init__():
-        "contrstructor for deck class"
+    def __init__(self):
+        """
+        Constructor of the deck, creates 52 standard playing cards and shuffles them
+        """
+        self.cards = list()
 
-    def shuffle(self):
-        """Function to randomize the position of cards in the deck (shuffle)"""
+        suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+
+        for i in range(len(suits)):
+            for j in range(2, 15):
+                self.cards.append(Card(j, suits[i]))
+
+        shuffle(self.cards)
+
