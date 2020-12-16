67e6633b1b31b0a49f524922d624302a8e584b9f
Jude Poole Jr <judepoolejr@gmail.com>
2020-12-15 21:23:46 -0500

    added functions to blackjack_test.py

--- blackjack_test.py
+++ blackjack_test.py
@@ -1,3 +1,23 @@
-import pytest
+from blackjack import Blackjack
 
+def test_player_stand():
+    """Does player stand function when the isStand variable is set"""
+    blackjack_test = Blackjack()
+    blackjack_test.player_stand()
+    assert blackjack_test.player_isStand == True
 
+def test_player_stand_split():
+    """Does player stand function when the isStand variable is set for the split condition, where both the first
+    and second hand are currently standing"""
+    blackjack_test = Blackjack()
+    blackjack_test.player_isSplit = True
+    blackjack_test.player_firstStand = True
+    blackjack_test.player_secondStand = True
+    blackjack_test.player_stand()
+    assert blackjack_test.player_isStand == True
+
+def test_dealer_stand():
+    """Does dealer stand function when dealer_stand function is ran"""
+    blackjack_test = Blackjack()
+    blackjack_test.dealer_stand()
+    assert blackjack_test.dealer_isStand == True
